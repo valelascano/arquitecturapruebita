11c59389c58f2b30246147fd5771dcdf118962d8
valelascano <vale.lascano0603@gmail.com>
2023-03-29 18:48:14 -0300

    Commit incial
    AJSDBKSBDF

---
+++ pruebita.py
@@ -0,0 +1,18 @@
+#Nombrecitos
+
+nombre_1 = "Abrilchu"
+
+nombre_2 = "Nikito"
+
+nombre_3 = "Valenchu"
+
+nombre_4 = "Santino"
+
+nombre_5 = "Carolaina"
+
+
+print(nombre_1, "es una loka")
+print(nombre_2, "es una loka")
+print(nombre_3, "es una loka")
+print(nombre_4, "es una loka")
+print(nombre_5, "es una loka")
