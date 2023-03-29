26e721da7edae95917d7e876874be14977523e2f
valelascano <vale.lascano0603@gmail.com>
2023-03-29 18:57:06 -0300

    Update nuevo Vale
    sdsd

--- pruebita.py
+++ pruebita.py
@@ -17,4 +17,4 @@ print(nombre_3, "es una loka")
 print(nombre_4, "es una loka")
 print(nombre_5, "es una loka")
 
-# puto el que lee atte: abril rama main 18:53
+# Cambio de comentario en rama main Vale 18:56
